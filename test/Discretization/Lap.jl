06e062f92bfe41c8263b4240dc6216f3675ec78f
Carl Timothy Kelley <rootfndr@gmail.com>
2025-11-19 11:31:00 -0500

    Updates

--- test/Discretization/Lap.jl
+++ test/Discretization/Lap.jl
@@ -1,4 +1,9 @@
-function Lap(n=15,p=0.0)
+function Lap(n=15,p=0.0
+   onesok = Lap_one(n,p)
+   twook = Lap_two(n,p)
+end
+
+function Lap_one(n=15,p=0.0)
      rhs=zeros(n*n)
      uexfun(x,p) = 1.0
      uev = uexfun.(rhs,p)
@@ -8,3 +13,31 @@ function Lap(n=15,p=0.0)
      lapok = (nerr < 1.e-14)
      return lapok
 end
+
+function Lap_two(n=15, p=0.0)
+  h=1.0/(n+1.0)
+  X=h:h:1.0-h
+  uex = [u2d(x, y, p) for x in X, y in X]
+  lex = [Lapud2(x,y) for x in X, y in X]
+  rhs = reshape(uex, (n*n,1))
+  lex1d = reshape(lex,(n*n,1))
+  D2 = Lap2d(n)
+  lexap=D2*rhs
+  derr = norm(lexap - lex1d,Inf)
+  println(derr)
+end 
+
+#
+# Easy 2d test problem
+#
+function u2d(x,y,p)
+  u2d = x * exp(y)
+  return u2d
+end
+
+#
+# negative Laplacian of u2d
+#
+function Lapud2(x,y)
+  lu2d = -x*exp(y)
+end 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Test, Grad_Des_CKW
+using SIAMFANLEquations
+using SIAMFANLEquations.Examples
 import Grad_Des_CKW: LSolve
+import SIAMFANLEquations.Examples: Lap2d
 
 include("Discretization/Lap.jl")
 
