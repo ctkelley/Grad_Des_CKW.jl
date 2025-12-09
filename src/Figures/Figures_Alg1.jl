dea9cfc6cb4cbd774f786e4e169bebe64f4a5935
Carl Timothy Kelley <rootfndr@gmail.com>
2025-12-09 11:11:47 -0500

    more updates

--- src/Figures/Figures_Alg1.jl
+++ src/Figures/Figures_Alg1.jl
@@ -1,7 +1,7 @@
 function Figure1_2a(n=15; maxit=20000, p=.5)
 (n > 15) && (maxit=10000)
 tau_test1(n;maxit=maxit)
-ts=L"\\tau_0"
+ts=L"\tau_0"
 tautex="testing "*ts*". n=$n, p=$p"
 println(tautex)
 #title("Alg 1; testing tau. n=$n, p=$p")
@@ -10,8 +10,11 @@ end
 
 function Figure1_2b(n=15; maxit=20000, tau0=.01)
 (n > 15) && (maxit=10000)
+ts=L"\tau_0"
+ptex="Alg 1; testing p. n=$n," *ts* "=$tau0"
 p_test1(n; maxit=maxit)
-title("Alg 1; testing p. n=$n, tau0=$tau0")
+title(ptex)
+#title("Alg 1; testing p. n=$n, tau0=$tau0")
 end
 
 
