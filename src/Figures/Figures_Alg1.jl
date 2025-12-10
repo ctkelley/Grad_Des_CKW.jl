08b244b991124c15f09505e6515b7540790a8990
ctkelley <ctk@ncsu.edu>
2025-12-10 11:28:16 -0500

    Notebook complete for Alg 1 + Example 1

--- src/Figures/Figures_Alg1.jl
+++ src/Figures/Figures_Alg1.jl
@@ -1,5 +1,5 @@
 function Figure1_2a(n=15; maxit=20000, p=.5)
-(n > 15) && (maxit=10000)
+(n > 15) && (maxit=100000)
 tau_test1(n;maxit=maxit)
 ts=L"\tau_0"
 tautex="testing "*ts*". n=$n, p=$p"
@@ -9,7 +9,7 @@ title(tautex)
 end
 
 function Figure1_2b(n=15; maxit=20000, tau0=.01)
-(n > 15) && (maxit=10000)
+(n > 15) && (maxit=100000)
 ts=L"\tau_0"
 ptex="Alg 1; testing p. n=$n," *ts* "=$tau0"
 p_test1(n; maxit=maxit)
