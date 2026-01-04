de046beeb77a215415270ff62909b9d05366f5b3
Tim Kelley <ctk@ncsu.edu>
2026-01-04 14:50:42 -0500

    formatting

--- src/Figures/Example1.jl
+++ src/Figures/Example1.jl
@@ -2,14 +2,14 @@
 # Make the figures from example 1 for showing
 # in a notebook or the REPL
 #
-function Example1(n=15; maxit=20000)
-println("Figure 1 (a) and (b)")
-figure(2)
-n=15
-subplot(1,2,1)
-tau_test1(n; nu = 0.5, p = 0.5, maxit = maxit)
-PyPlot.title("Fig 1 (a)")
-subplot(1,2,2)
-p_test1(n; nu = 0.5, tau0 = 0.01, algfun=alg1)
-PyPlot.title("Fig 1 (b)")
+function Example1(n = 15; maxit = 20000)
+    println("Figure 1 (a) and (b)")
+    figure(2)
+    n=15
+    subplot(1, 2, 1)
+    tau_test1(n; nu = 0.5, p = 0.5, maxit = maxit)
+    PyPlot.title("Fig 1 (a)")
+    subplot(1, 2, 2)
+    p_test1(n; nu = 0.5, tau0 = 0.01, algfun = alg1)
+    PyPlot.title("Fig 1 (b)")
 end
--- src/Figures/Figures_CKW.jl
+++ src/Figures/Figures_CKW.jl
@@ -1,56 +1,53 @@
-function Figure1_2a(n=15; maxit=20000, p=.5)
-(n > 15) && (maxit=100000)
-tau_test1(n;maxit=maxit)
-ts=L"\tau_0"
-tautex="testing "*ts*". n=$n, p=$p"
-println(tautex)
-#title("Alg 1; testing tau. n=$n, p=$p")
-title(tautex)
+function Figure1_2a(n = 15; maxit = 20000, p = 0.5)
+    (n > 15) && (maxit=100000)
+    tau_test1(n; maxit = maxit)
+    ts=L"\tau_0"
+    tautex="testing "*ts*". n=$n, p=$p"
+    println(tautex)
+    #title("Alg 1; testing tau. n=$n, p=$p")
+    title(tautex)
 end
 
-function Figure1_2b(n=15; maxit=20000, tau0=.01)
-(n > 15) && (maxit=100000)
-ts=L"\tau_0"
-ptex="Alg 1; testing p. n=$n," *ts* "=$tau0"
-p_test1(n; maxit=maxit)
-title(ptex)
-#title("Alg 1; testing p. n=$n, tau0=$tau0")
+function Figure1_2b(n = 15; maxit = 20000, tau0 = 0.01)
+    (n > 15) && (maxit=100000)
+    ts=L"\tau_0"
+    ptex="Alg 1; testing p. n=$n," * ts * "=$tau0"
+    p_test1(n; maxit = maxit)
+    title(ptex)
+    #title("Alg 1; testing p. n=$n, tau0=$tau0")
 end
 
-function Figure3(n=15; maxit=20000, tau0=.1)
-(n > 15) && (maxit=100000)
-ts=L"\tau_0"
-ptex="Alg 2; testing p. n=$n," *ts* "=$tau0"
-p_test1(n; maxit=maxit, algfun=alg2e1)
-title(ptex)
-#title("Alg 2; testing p. n=$n, tau0=$tau0")
+function Figure3(n = 15; maxit = 20000, tau0 = 0.1)
+    (n > 15) && (maxit=100000)
+    ts=L"\tau_0"
+    ptex="Alg 2; testing p. n=$n," * ts * "=$tau0"
+    p_test1(n; maxit = maxit, algfun = alg2e1)
+    title(ptex)
+    #title("Alg 2; testing p. n=$n, tau0=$tau0")
 end
 
-function Figure4_5a(n=15; maxit=20000, p=.5, 
-    tauvec=[0.2, 0.1, 0.05, 0.01])
-(n > 15) && (maxit=100000)
-tau_test1(n;maxit=maxit, algfun=alg3e1, tauvec=tauvec)
-ts=L"\tau_0"
-tautex="testing "*ts*". n=$n, p=$p"
-println(tautex)
-#title("Alg 3; testing tau. n=$n, p=$p")
-title(tautex)
+function Figure4_5a(n = 15; maxit = 20000, p = 0.5, tauvec = [0.2, 0.1, 0.05, 0.01])
+    (n > 15) && (maxit=100000)
+    tau_test1(n; maxit = maxit, algfun = alg3e1, tauvec = tauvec)
+    ts=L"\tau_0"
+    tautex="testing "*ts*". n=$n, p=$p"
+    println(tautex)
+    #title("Alg 3; testing tau. n=$n, p=$p")
+    title(tautex)
 end
 
-function Figure4_5b(n=15; maxit=20000, tau0=.01)
-(n > 15) && (maxit=100000)
-ts=L"\tau_0"
-ptex="Alg 3; testing p. n=$n," *ts* "=$tau0"
-p_test1(n; maxit=maxit, algfun=alg3e1, tau0=tau0)
-title(ptex)
-#title("Alg 3; testing p. n=$n, tau0=$tau0")
+function Figure4_5b(n = 15; maxit = 20000, tau0 = 0.01)
+    (n > 15) && (maxit=100000)
+    ts=L"\tau_0"
+    ptex="Alg 3; testing p. n=$n," * ts * "=$tau0"
+    p_test1(n; maxit = maxit, algfun = alg3e1, tau0 = tau0)
+    title(ptex)
+    #title("Alg 3; testing p. n=$n, tau0=$tau0")
 end
 
-function Figure6ab(n=15; maxit=1000, tau0=20.0, p=.5)
-ts=L"\tau_0"
-ptex="Alg 3; grad norm vs error. p=$p," *ts* "=$tau0"
-res_test1(n; maxit=maxit, algfun=alg3e1, tau0=tau0, p=p)
-title(ptex)
+function Figure6ab(n = 15; maxit = 1000, tau0 = 20.0, p = 0.5)
+    ts=L"\tau_0"
+    ptex="Alg 3; grad norm vs error. p=$p," * ts * "=$tau0"
+    res_test1(n; maxit = maxit, algfun = alg3e1, tau0 = tau0, p = p)
+    title(ptex)
 end
-
-
