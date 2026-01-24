33930d71d211ab763fca8c99e263ba38e61df980
ctkelley <ctk@ncsu.edu>
2026-01-24 05:49:35 -0500

    Update for submission

--- src/Algorithms/alg2.jl
+++ src/Algorithms/alg2.jl
@@ -5,49 +5,45 @@ function alg2(u0, fobj, fgrad, proj, pdata, R, tau0, epsilon, maxit)
     mu=pdata.mu
     reshist=Float64[]
     errhist=Float64[]
-    N0=norm(R)
     push!(reshist, 1.0)
     push!(errhist, E/E0)
-    RX=copy(R)
     u = copy(u0)
-    ux=copy(u)
     fc = fobj(u, pdata)
-    reshist=Float64[]
-    errhist=Float64[]
-    N0=norm(R, Inf)
-    gnormc=norm(R)
-    push!(reshist, 1.0)
-    push!(errhist, E/E0)
-    N=length(u)
-#    tau=tau0/N
-    tau=tauev(tau0,N)
-    RX=copy(R)
-    ux=copy(u)
+    v = copy(u0)
+    ux = copy(u0)
+    vx = copy(u0)
+    R .= fgrad(v, pdata)
+    RX = copy(R)
+    N0=norm(R)
+    N = length(u)
+    #tau=tau0/N
+    tau=tauev(tau0, N)
+    f_best = fc
     for ix = 1:maxit
-        u .= proj0(u - tau * R)
-        fp = fobj(u, pdata)
-        df = fp - fc
+        v .= proj0(v - tau * R)
+        ft = fobj(v, pdata)
+        df = ft - fc
         # The sufficient decrease condition is WEAKER that simple decrease
         # because of the extra positive terms
-        SD1 = dot(R, u-ux) + 0.5 * dot(u-ux, u-ux)/tau + mu*epsilon^2/4
-        R .= fgrad(u, pdata)
-        gnormp=norm(R)
-        dg = gnormp - gnormc
+        SD1 = dot(RX, v - vx) + 0.5 * dot(v - vx, v - vx) / tau + mu * epsilon^2 / 4
+        R .= fgrad(v, pdata)
         if (df > SD1)
-            u .= ux
+            v .= vx
             R .= RX
             tau *= 0.5
         else
-            # Make sure that you get simple decrease after accepting the step
-            if (dg < 0)
-                RX .= R
-                gnorm=gnormp
-                #            R .= fgrad(u, pdata)
+            RX .= R
+            vx .= v
+            fc = ft
+            if (f_best < fc)
+                u .= ux
+            else
+                f_best = fc
+                u .= v
                 ux .= u
-                fc = fobj(u, pdata)
             end
         end
-        rrnrm=norm(R, Inf)/N0
+        rrnrm=norm(R)/N0
         push!(reshist, rrnrm)
         E=norm(u - uex)
         push!(errhist, E/E0)
